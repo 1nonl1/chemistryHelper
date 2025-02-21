92a083aca3a96af84255cd0b6cf02eddf3ef8835
1nonl1 <tobiasbraymen@gmail.com>
2025-02-21 17:27:06 +0000

    git commit

--- Periodic Table/main.py
+++ Periodic Table/main.py
@@ -18,7 +18,7 @@ def main():
     elif c1 == "e":
         balask = input("Do you want to balance the equation or predict products or both? (b/p/both): ")
         if balask == "b":
-            subprocess.call(["python", "balance.py"])
+            subprocess.call(["python", "Periodic Table/balance.py"])
         elif balask == "p":
             print("This feature is not yet available.")
             print("If you have any sugestions please say so in the discussions tab in the github page.")
